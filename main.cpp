ebb2b9fdeaf3b5e727cec534a91be93b5c84f717
Ben White <ben@benwhite.au>
2025-11-03 16:51:33 +1100

    added function to natively find the median of a test array of random numbers and cleaned up print statements to verify if algos were successful

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <random>
+#include <string>
+#include <algorithm>
 #include "median.hpp"
 
 //generates a random array of n integers between f and l
@@ -16,22 +18,39 @@ std::vector<int> generate_array(int f, int l, int n, std::mt19937& rng){
     return test; //return generated vector
 }
 
+//turn test vector into string for printing
+std::string vec_str(const std::vector<int>& vec) {
+    std::string result = "[";
+    const char* space = "";
+    for (int x : vec) {
+        result += space + std::to_string(x);
+        space = ", ";
+    }
+    result += "]";
+    return result; //return string representation of vector
+}
+
+//finds median using native nth_element for test validation
+int nth_median(std::vector<int> vector) {
+    const std::size_t k = (vector.size() - 1) / 2; //kth element aka the index of the median, if n is even then lower median
+    std::nth_element(vector.begin(), vector.begin() + k, vector.end()); //partially sorts v so kth is at correct index
+    return vector[k]; //return median or lower median value
+}
+
 int main() {
     std::mt19937 rng(23); //random no. generator, fixed seed for reproducibility
     std::vector<int> test = generate_array(1, 100, 15, rng); //generate randomised test array
 
-    //print test array
-    std::cout << "Test array: [";
-    const char* space = "";
-    for (int x : test) {
-        std::cout << space << x;
-        space = ", ";
-    }
-    std::cout << "]\n";
+    int median = nth_median(test); //find median of test array using native nth_element
+ 
+    std::cout << "Test array: " << vec_str(test) << "\n"; //print test array
 
     int q_median = qselect(test, true); //return median using quick select
-    std::cout << "Quick Select: " << q_median << "\n";
-
     int m_median = mom(test, false); //return median using median of medians
-    std::cout << "Median of Medians: " << m_median << "\n";
+    
+    std::cout << ((q_median == median) ? "Success! " : "Failed! ");     
+    std::cout << "Expected: " << median << " Quick Select: " << q_median << "\n";
+
+    std::cout << ((m_median == median) ? "Success! " : "Failed! ");
+    std::cout << "Expected: " << median << " Median of Medians: " << m_median << "\n";
 }
