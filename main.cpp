2d9dfb59379c57a82d68c2aa52b1abe2e2ef5424
Ben White <ben@benwhite.au>
2025-11-03 16:21:03 +1100

    added random array generator and print out the test array

--- main.cpp
+++ main.cpp
@@ -1,13 +1,37 @@
 #include <iostream>
 #include <vector>
+#include <random>
 #include "median.hpp"
 
-int main() {
-    std::vector<int> numbers = {10, 3, 4, 9, 6, 1, 2, 8, 5, 7}; //test array
+//generates a random array of n integers between f and l
+std::vector<int> generate_array(int f, int l, int n, std::mt19937& rng){
+    std::uniform_int_distribution<int> dist(f, l); //defines a range between f and l
+    std::vector<int> test;
 
-    int q_median = qselect(numbers, true); //return median using quick select
+    //generates n random integers within range and adds to vector
+    for (int i = 0; i < n; ++i) {
+        test.push_back(dist(rng)); 
+    }
+
+    return test; //return generated vector
+}
+
+int main() {
+    std::mt19937 rng(23); //random no. generator, fixed seed for reproducibility
+    std::vector<int> test = generate_array(1, 100, 15, rng); //generate randomised test array
+
+    //print test array
+    std::cout << "Test array: [";
+    const char* space = "";
+    for (int x : test) {
+        std::cout << space << x;
+        space = ", ";
+    }
+    std::cout << "]\n";
+
+    int q_median = qselect(test, true); //return median using quick select
     std::cout << "Quick Select: " << q_median << "\n";
 
-    int m_median = mom(numbers, false); //return median using median of medians
+    int m_median = mom(test, false); //return median using median of medians
     std::cout << "Median of Medians: " << m_median << "\n";
 }
